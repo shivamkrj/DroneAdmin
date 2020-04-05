7dabf94ac529d550de7e3cc7077a589136bc3450
shad0w1947 <thakurankush947@gmail.com>
2020-04-05 18:23:44 +0530

    ngo firebaseconnect

--- app/src/main/java/in/shivamkrj/droneadmin/ListNgo.java
+++ app/src/main/java/in/shivamkrj/droneadmin/ListNgo.java
@@ -1,22 +1,29 @@
 package in.shivamkrj.droneadmin;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListNgo extends AppCompatActivity {
 
@@ -24,6 +31,9 @@ public class ListNgo extends AppCompatActivity {
     NgoAdapter adapter;
     ArrayList<NgoData> ngoDataArrayList;
     ViewClickInterface viewClickInterface;
+    DatabaseReference ngoListReference;
+    ProgressDialog pd;
+    FirebaseDatabase database;
 
 
     @Override
@@ -32,9 +42,48 @@ public class ListNgo extends AppCompatActivity {
         setContentView(R.layout.activity_list_ngo);
 
         ngoDataArrayList=new ArrayList<>();
-        for(int i=0;i<15;i++)ngoDataArrayList.add(new NgoData(i+"",i+"","9012384858","cdcd"));
+
+        database = FirebaseDatabase.getInstance();
+        ngoListReference = database.getReference("NGOLISTS");
+
+        for(int i=0;i<15;i++){
+            ngoDataArrayList.add(new NgoData(i+"",i+"","9012384858","cdcd"));
+//            ngoListReference.push().setValue(ngoDataArrayList.get(i));
+
+        }
+        fetchData();
         setRecyclerView();
 
+    }
+
+    private void fetchData() {
+        pd = new ProgressDialog(this);
+        pd.setMessage("Loading ...");
+        pd.setCanceledOnTouchOutside(false);
+        pd.show();
+        ngoDataArrayList.clear();
+        ngoListReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot > dataSnapshotIterable = dataSnapshot.getChildren();
+                if(dataSnapshotIterable==null)return;
+                Log.i("fetch",dataSnapshot.getChildren().toString()+" "+dataSnapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot: dataSnapshotIterable) {
+//                    Log.i("data111",postSnapshot.getValue()+"");
+                    ngoDataArrayList.add(postSnapshot.getValue(NgoData.class));
+
+                    Log.i("fetch",ngoDataArrayList.size()+"");
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        Log.i("fetch",ngoDataArrayList.size()+"");
+        pd.dismiss();
     }
 
     private void setRecyclerView() {
--- app/src/main/java/in/shivamkrj/droneadmin/NgoData.java
+++ app/src/main/java/in/shivamkrj/droneadmin/NgoData.java
@@ -1,14 +1,17 @@
 package in.shivamkrj.droneadmin;
 
 public class NgoData {
-    String ngoName;
-    String userName;
-    String phoneNUmber;
-    String  detail;
+    public String ngoName;
+    public String userName;
+    public String phoneNUmber;
+    public String  detail;
     public NgoData(String ngoName,String userName ,String phoneNUmber,String detail){
         this.detail=detail;
         this.ngoName=ngoName;
         this.phoneNUmber=phoneNUmber;
         this.userName=userName;
+    }
+    public NgoData(){
+
     }
 }
